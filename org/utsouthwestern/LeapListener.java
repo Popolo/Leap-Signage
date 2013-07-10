048b85be661eb35c4905d43cf51a08165fde9ced
C. K. Vernino <cvernino@tx.rr.com>
2013-07-10 11:18:44 -0500

    Updating with sdk.

--- org/utsouthwestern/LeapListener.java
+++ org/utsouthwestern/LeapListener.java
@@ -5,7 +5,6 @@ import java.awt.event.InputEvent;
 
 import com.leapmotion.leap.*;
 
-@SuppressWarnings("unused")
 public class LeapListener extends Listener {
 	Robot mouse;
 	Screen screen;
@@ -17,15 +16,13 @@ public class LeapListener extends Listener {
 	
 	final float SHAKE_BOUND = 12.0f;
 	final int TIMER_REQ = 300;
+	final float PINCH_BOUND = 5.0f;
 	
 	public void onInit(Controller leap){
 		try {
 			mouse = new Robot();
 			screen = leap.locatedScreens().get(0);
 			leap.enableGesture(Gesture.Type.TYPE_SWIPE);
-		    //leap.enableGesture(Gesture.Type.TYPE_CIRCLE);
-		    leap.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
-		    //leap.enableGesture(Gesture.Type.TYPE_KEY_TAP);
 		    buttonUp = true;
 		    prevPos = new Vector(0,0,0);
 		} catch (AWTException e) {
@@ -35,22 +32,17 @@ public class LeapListener extends Listener {
 	
 	public void onFrame(Controller leap){
 		Frame cFrame = leap.frame();
-    	Pointable pointer = cFrame.pointables().get(0);
+    	Pointable pointer = cFrame.pointables().frontmost();
     	
     	//Instructions dependent on presence of pointables
     	if(pointer.isValid()){
-    		Vector v = mouseSet(pointer);
+    		Vector v = mouseFind(pointer);
     		readGestures(cFrame.gestures(),pointer);
     		
     		//Click if the user has hovered long enough.
-    		if(pointClick(v)){
-    			mouse.mouseMove((int) prevPos.getX(), (int) prevPos.getY());
-    			mouse.mousePress(InputEvent.BUTTON1_MASK);
-				mouse.mouseRelease(InputEvent.BUTTON1_MASK);
-    		}
-    		if(cFrame.pointables().count() == 1 && pinchability(prevDist, cFrame.pointables())){
-    			setPinch(cFrame);
-    		}
+    		pointClick2(pointClick(v));
+    		
+    		pinch(cFrame);
     	}
 	}
 	
@@ -59,11 +51,6 @@ public class LeapListener extends Listener {
 		for(int i=0;i<gestures.count();i++){
 			Gesture current = gestures.get(i);
 			switch(current.type()){
-			case TYPE_SCREEN_TAP:
-				mouse.mousePress(InputEvent.BUTTON1_MASK);
-				mouse.mouseRelease(InputEvent.BUTTON1_MASK);
-				System.out.println("touch");
-				break;
 			case TYPE_SWIPE:
 				if(buttonUp){
 					mouse.mousePress(InputEvent.BUTTON1_MASK);
@@ -84,31 +71,44 @@ public class LeapListener extends Listener {
 		}
 	}
 	
-	private Vector mouseSet(Pointable pointer){
+	private Vector mouseFind(Pointable pointer){
 		Vector v = screen.intersect(pointer, true);
 		int x = (int) (v.getX() * screen.widthPixels());
 		int y = 1080 - (int) (v.getY() * screen.heightPixels());
-	//	mouse.mouseMove(x, y);
 		Vector i = new Vector(x,y,0);
 		return i;
 	}
 	
-	private void pinchTest(PointableList pointables){
-		if(pointables.count()>1 && !buttonUp){
-			mouse.mousePress(InputEvent.BUTTON1_MASK);
-			buttonUp = true;
-		}else if(pointables.count()<=1 && buttonUp){
-			mouse.mouseRelease(InputEvent.BUTTON1_MASK);
-			buttonUp = false;
+	void pinch(Frame frame){
+		PointableList pointables = frame.pointables();
+		Pointable pointer = pointables.frontmost();
+		if (pointables.count() == 2){
+			Pointable other = null;
+			for (int i = 0; i < pointables.count(); i++){
+				if(pointables.get(i) != pointer){
+					other = pointables.get(i);
+					break;
+				}
+			}
+			if(pointer.stabilizedTipPosition().distanceTo(other.stabilizedTipPosition()) < PINCH_BOUND){
+				canPinch = true;
+			}else{
+				canPinch = false;
+			}
+		}else if(pointables.count() == 1){
+			if(canPinch){
+				mouse.mousePress(InputEvent.BUTTON1_MASK);
+			}else{
+				mouse.mouseRelease(InputEvent.BUTTON1_MASK);
+			}
+		}else{
+			canPinch = false;
 		}
 	}
 	
-	private boolean pinchability(float distPrev, PointableList pointables){
-		float distCurrent = screen.intersect(pointables.get(0), true).distanceTo(screen.intersect(pointables.get(1), true));
-		prevDist = distCurrent;
-		return (distCurrent < distPrev);
-	}
 	
+	
+	// the hoverClicking methods.
 	private boolean pointClick(Vector current){
 		if(current.distanceTo(prevPos) > SHAKE_BOUND){
 			timer = 0;
@@ -127,9 +127,12 @@ public class LeapListener extends Listener {
 		}
 	}
 	
-	public void setPinch(Frame f){
-		if(!(f.pointables().count() == 1)){
-			canPinch = false;
+	private void pointClick2(boolean yay){
+		if (yay){
+			mouse.mouseMove((int) prevPos.getX(), (int) prevPos.getY());
+			mouse.mousePress(InputEvent.BUTTON1_MASK);
+			mouse.mouseRelease(InputEvent.BUTTON1_MASK);
+			timer = 0;
 		}
 	}
 }
